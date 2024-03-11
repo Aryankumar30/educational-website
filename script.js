9bfdd464e17c6aa007a67b5372628ff4bdb44aa8
Aryankumar30 <103895665+Aryankumar30@users.noreply.github.com>
2024-03-11 23:14:52 +0530

    added initial files

---
+++ script.js
@@ -0,0 +1,18 @@
+
+function scrollAppear() {
+  var introText = document.querySelector('.side-text');
+  var sideImage = document.querySelector('.sideImage');
+  var introPosition = introText.getBoundingClientRect().top;
+  var imagePosition = sideImage.getBoundingClientRect().top;
+  
+  var screenPosition = window.innerHeight / 1.2;
+
+  if(introPosition < screenPosition) {
+    introText.classList.add('side-text-appear');
+  }
+  if(imagePosition < screenPosition) {
+    sideImage.classList.add('sideImage-appear');
+  }
+}
+
+window.addEventListener('scroll', scrollAppear);
